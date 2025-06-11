b10c572f3d0bdeb604b0982e6f83edea4f08a637
eyurekli <ekin.yurekli@trench-group.com>
2025-06-11 15:31:00 -0400

    Folder structure set

---
+++ README.md
@@ -0,0 +1,9 @@
+# How to Run
+
+install anaconda
+
+conda create -n llmapp python=3.8 -y
+
+conda activate llmapp
+
+pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version= "0.1.0",
+    author= "Ekin Yurekli",
+    author_email = "ekin.y@dal.ca",
+    packages = find_packages(),
+    install_requires=[]
+)
